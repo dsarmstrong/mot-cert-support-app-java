93a1c8abbe2a626783e1e0d811a6b3ec52ff5373
dsarmstrong <darmstrong@naic.org>
2024-04-22 08:48:00 -0500

    Added reporting to Github Actions

---
+++ src/test/java/AuthPayload.java
@@ -0,0 +1,22 @@
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class AuthPayload {
+
+    @JsonProperty
+    private String email;
+    @JsonProperty
+    private String password;
+
+    public AuthPayload(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,11 @@
+package com.ministryoftesting;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+
+    @Test
+    public void shellTest() {
+        System.out.println("This is a shell test");
+    }
+}
